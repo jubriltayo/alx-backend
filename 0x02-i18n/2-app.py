0503594966930d0444dd5dcb3c098d71d184e287
Tayo Jubril <jubriltayo@gmail.com>
2023-10-31 20:30:31 +0100

    task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -18,6 +18,7 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
+    """select best match from languages"""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
