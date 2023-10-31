0e36001b865f90526ca5c44f0e0b82d368e15f8a
Tayo Jubril <jubriltayo@gmail.com>
2023-10-31 21:52:07 +0100

    task 4

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""This module sets up a flask app"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config():
+    """defines different languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """select best match from languages"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def home():
+    """renders index page"""
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
