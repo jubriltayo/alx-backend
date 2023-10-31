99a45f18558f9b591999d320d8ac5094dd356032
Tayo Jubril <jubriltayo@gmail.com>
2023-10-31 22:03:44 +0100

    task 4

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """This module sets up a flask app"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
 class Config():
